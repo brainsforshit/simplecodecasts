6694fa469f77806097ce6dbb30f323ac49a5c78b
Boris Callebert <callebert.boris@outlook.be>
2016-08-13 15:56:19 +0000

    Changed email

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-    default to: 'callebert.boris@outlook.be'
+    default to: 'hello@boriscallebert.com'
 
     def contact_email(name, email, body)
         @name = name
